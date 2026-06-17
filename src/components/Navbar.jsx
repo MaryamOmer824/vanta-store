c774577170163c59cd128a702328939675d0ef8f
MaryamOmer824 <maryamomer414@email.com>
2026-06-17 14:14:28 -0700

    Created navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>VANTA</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <button>
+        Cart
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
